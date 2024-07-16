b0debab44cf42e0165a4f229dd677413ea27143f
Bsubs <56755990+Bsubs@users.noreply.github.com>
2024-07-16 21:33:07 +0800

    Updated parameters in MTD AI Operation

--- mtdnetwork/operation/mtd_ai_training.py
+++ mtdnetwork/operation/mtd_ai_training.py
@@ -72,7 +72,7 @@ class MTDAITraining:
             state, time_series = self.get_state_and_time_series()
 
             # Static network degradation factor
-            if (self.env.now - self.network.get_last_mtd_triggered_time()) > 100: # The number 100 is just a temperory threshold
+            if (self.env.now - self.network.get_last_mtd_triggered_time()) > 2000:
                 action = 1
             else:
                 action = choose_action(state, time_series, self.main_network, 5, self.epsilon)
